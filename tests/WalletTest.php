820e9c9460c735641ba597471575b3face55c42e
Galystan <ambiyadms@gmail.com>
2025-11-27 09:25:04 +0100

    Add tests for wallet functionality: negative deposit handling and spending limits

--- tests/WalletTest.php
+++ tests/WalletTest.php
@@ -21,4 +21,32 @@ class WalletTest extends TestCase {
 
         $this->assertSame(50.0, $wallet->getBalance());
     }
+    // Galyst
+    public function testCannotDepositNegativeAmount(): void
+    {
+        $wallet = new Wallet();
+        $this->expectException(\InvalidArgumentException::class);
+        $wallet->deposit(-10);
+    }
+    // Galyst
+    public function testSpendDecreasesBalance(): void
+    {
+        $wallet = new Wallet();
+        $wallet->deposit(100.0);
+
+        $wallet->spend(20.0, 'Test');
+
+        $this->assertSame(80.0, $wallet->getBalance());
+    }
+    // Galyst
+    public function testCannotSpendMoreThanBalance(): void
+    {
+        $wallet = new Wallet();
+        $wallet->deposit(10.0);
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("Fonds insuffisants");
+
+        $wallet->spend(50.0);
+    }
 }
